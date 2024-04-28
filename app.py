bf7637c0ccfe3cf38a7d88d09036ad6778b8a1ed
Matheus Graciki <matheusgraciki7@gmail.com>
2024-04-27 21:29:17 -0300

    feature: resize the window and change the variable names

--- app.py
+++ app.py
@@ -1,28 +1,38 @@
 import cv2
-def displayVideo(video_path):
-    cap = cv2.VideoCapture(video_path)
 
-    if not cap.isOpened():
-        print('Error: the video cannot be opened.')
+def resizeFrame(frame, scale=0.3):
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+    dimensions = (width, height)
+
+    return cv2.resize(frame, dimensions, interpolation=cv2.INTER_AREA)
+
+
+def displayVideo(video_path):
+    capture = cv2.VideoCapture(video_path)
+    
+    if not capture.isOpened():
+        print('Error: o vídeo não pode ser aberto.')
         return
 
-    cv2.namedWindow('camera capture')
+    cv2.namedWindow('plates')
 
     while True:
-        ret, frame = cap.read()
+        frameReadSucess, frame = capture.read()
 
-        if not ret:
-            print('Failed to read the frame')
+        if not frameReadSucess:
+            print('Erro: A gravação do chegou ao fim')
             break
 
-        cv2.imshow('highway traffic', frame)
+        resizedFrame = resizeFrame(frame)
+        cv2.imshow('highway traffic', resizedFrame)
 
-        # Press "q" to exit the window
+        # Press "q" to close the window
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-    cap.release()
+    capture.release()
     cv2.destroyAllWindows()
 
-# call the functions
-displayVideo("video.mov")
+
+displayVideo("video01.mp4")
