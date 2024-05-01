f6c3186f3e647b9e47ab7813863b10cab76088ef
Matheus Graciki <85004422+MatheusGraciki@users.noreply.github.com>
2024-05-01 20:09:33 -0300

    feature: detect plates on video and drawn a line arount it; and, crop and save the license plate image

--- app.py
+++ app.py
@@ -1,38 +1,61 @@
+import numpy as np
+import supervision as sv
+from ultralytics import YOLO
 import cv2
+import os
 
-def resizeFrame(frame, scale=0.3):
-    width = int(frame.shape[1] * scale)
-    height = int(frame.shape[0] * scale)
-    dimensions = (width, height)
+# Initialize the model for plate reading, tracking, and drawing a box around the plate.
+model = YOLO("drive/MyDrive/Colab/license_plate_detector.pt")
+tracker = sv.ByteTrack(track_activation_threshold=0.60)
+box_annotator = sv.BoundingBoxAnnotator()
 
-    return cv2.resize(frame, dimensions, interpolation=cv2.INTER_AREA)
+# Directory to save cropped image files.
+output_dir = 'drive/MyDrive/Colab/output'
 
+# Set to store unique tracker IDs of cropped plates.
+cropped_track_ids = set()
 
-def displayVideo(video_path):
-    capture = cv2.VideoCapture(video_path)
-    
-    if not capture.isOpened():
-        print('Error: o vídeo não pode ser aberto.')
-        return
+def crop_image(image: np.ndarray, xyxy: np.ndarray) -> np.ndarray:
+    """Crops the region of interest from the image."""
+    xyxy = np.round(xyxy).astype(int)
+    x1, y1, x2, y2 = xyxy
+    cropped_img = image[y1:y2, x1:x2]
+    return cropped_img
 
-    cv2.namedWindow('plates')
+def callback(frame: np.ndarray, frame_number: int) -> np.ndarray:
+    """Processes each frame of the video."""
+    global cropped_track_ids
 
-    while True:
-        frameReadSucess, frame = capture.read()
+    # Detect objects in the frame using the license plate recognition model.
+    results = model(frame)[0]
+    detections = sv.Detections.from_ultralytics(results)
+    detections = tracker.update_with_detections(detections)
 
-        if not frameReadSucess:
-            print('Erro: A gravação do chegou ao fim')
-            break
+    for i, xyxy in enumerate(detections.xyxy):
+        # Check if there are tracker IDs.
+        if detections.tracker_id.size > 0:
+            # Get the tracker ID of the detection.
+            track_id = detections.tracker_id[i]
+            # Check if the car plate in the image has already been cropped or not, based on the tracking id.
+            if track_id not in cropped_track_ids:
+                # Crop the region of interest (car plate).
+                cropped_region = crop_image(frame, xyxy)
 
-        resizedFrame = resizeFrame(frame)
-        cv2.imshow('highway traffic', resizedFrame)
+                # Create a unique image name based on the tracking id and the detection index.
+                image_name = f"{track_id}_TrackId_detection_{i}.jpg"
+          
+                # Save the cropped region as an image.
+                cv2.imwrite(os.path.join(output_dir, image_name), cropped_region)
 
-        # Press "q" to close the window
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+                # Add the tracker ID to the set of IDs that have already had the image cropped.
+                cropped_track_ids.add(track_id)
 
-    capture.release()
-    cv2.destroyAllWindows()
+    # Draw a box around the car plate.
+    return box_annotator.annotate(frame.copy(), detections=detections)
 
-
-displayVideo("video01.mp4")
+# Process the video with the callback function.
+sv.process_video(
+    source_path="drive/MyDrive/Colab/video02.mp4",
+    target_path="drive/MyDrive/Colab/result.mp4",
+    callback=callback
+)
